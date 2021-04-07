966e53ef86ee1b1b0bf8c835faeff61563ef6fcb
torrestor <torres.jovem@gmail.com>
2021-04-06 23:41:12 -0300

    execução script

---
+++ mysqlScript/01.sql
@@ -0,0 +1,2 @@
+use desafio_db_node;
+create table people  (id int not null auto_increment, name varchar(255), primary key(id));
--- mysqlScript/CreateTablePeople.sql
+++
@@ -1,6 +0,0 @@
-use desafio_db_node;
-create table people  (
-    id int not null auto_increment, 
-    name varchar(255), 
-    primary key(id)
-);
